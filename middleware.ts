f9d5c70dc36796a19e66932119133925b8da62ef
Ritvik Shukla <ritvikshukla261@gmail.com>
2024-02-28 20:41:56 +0530

    testing on preview

--- middleware.ts
+++ middleware.ts
@@ -41,5 +41,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|connect).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.png|connect).*)'],
 }
