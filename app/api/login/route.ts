8e84122240dfab350ad4856ae7fcaf408ec5fc78
Ritvik Shukla <ritvikshukla261@gmail.com>
2024-02-28 21:48:38 +0530

    testing on preview

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -12,14 +12,14 @@ export async function POST(request: NextRequest, response: NextResponse) {
       const idToken = authorization.split('Bearer ')[1]
       const decodedToken = await auth().verifyIdToken(idToken)
       if (decodedToken) {
-        const expiresIn = 60 * 60 * 24 * 5 * 1000
+        const expiresIn = 60 * 60 * 5 * 1000
         const sessionCookie = await auth().createSessionCookie(idToken, {
           expiresIn,
         })
         const options = {
           name: 'session',
           value: sessionCookie,
-          maxAge: expiresIn,
+          maxAge: expiresIn / 1000,
           httpOnly: true,
         }
 
@@ -34,13 +34,13 @@ export async function POST(request: NextRequest, response: NextResponse) {
 }
 
 export async function GET(request: NextRequest) {
-  const session = cookies().get('session')?.value || ''
+  const session = request.cookies.get('session') || ''
 
   if (!session) {
     return NextResponse.json({ isLogged: false }, { status: 401 })
   }
 
-  const decodedClaims = await auth().verifySessionCookie(session, true)
+  const decodedClaims = await auth().verifySessionCookie(session.value, true)
 
   if (!decodedClaims) {
     return NextResponse.json({ isLogged: false }, { status: 401 })
